0a6a59ca366e080e4d29138d2239f2f2736def38
Shreyas Girjapure <66116603+shreyas-girjapure@users.noreply.github.com>
2023-12-27 12:35:58 +0000

    saving with errors

--- index.js
+++ index.js
@@ -1,11 +1,81 @@
 import flowSampleMetadata from "./samples/flowImport.js";
 import { removeKeysFromObject } from "./transformationUtilities/utils.js";
 
-const nodesNotAllowed = ["choices","constants","dynamicChoiceSets","formulas","environments","processMetadataValues","variables","apexPluginCalls","collectionProcessors","description","interviewLabel","isAdditionalPermissionRequiredToRun","isOverridable","isTemplate","loops","migratedFromWorkflowRuleName","orchestratedStages","overriddenFlow","processType","recordRollbacks","runInMode","sourceTemplate","startElementReference","status","steps","textTemplates","timeZoneSidKey","transforms","triggerOrder","urls","waits"];
-
-const nodesAllowed = ["actionCalls","assignments","decisions","recordCreates","recordDeletes","recordLookups","recordUpdates","screens","start","subflows"];
+const nodesNotAllowed = ["choices", "constants", "dynamicChoiceSets", "formulas", "environments", "processMetadataValues", "variables", "apexPluginCalls", "collectionProcessors", "description", "interviewLabel", "isAdditionalPermissionRequiredToRun", "isOverridable", "isTemplate", "loops", "migratedFromWorkflowRuleName", "orchestratedStages", "overriddenFlow", "processType", "recordRollbacks", "runInMode", "sourceTemplate", "startElementReference", "status", "steps", "textTemplates", "timeZoneSidKey", "transforms", "triggerOrder", "urls", "waits"];
 
 
-let flowNode = removeKeysFromObject(flowSampleMetadata,nodesNotAllowed);
-console.log(flowNode);
+let flowNode = removeKeysFromObject(flowSampleMetadata, nodesNotAllowed);
 
+function analyzeFlowNodes(flowNode) {
+    let entriesOfFlowNode = Object.entries(flowNode);
+    let countOfNodeVsIncomingNode = {};
+
+    for (let [key, value] of entriesOfFlowNode) {
+
+        if (key === "decisions") {      
+            Array.from(value).forEach(item => {
+                let targetRefName = item?.defaultConnector?.targetReference;
+                let nodeName = item?.name;
+
+                let listOfRules = item?.rules;
+                Array.from(listOfRules).forEach(item => {
+                    let ruleRefName = item?.connector?.targetReference;
+                    if (countOfNodeVsIncomingNode.hasOwnProperty(ruleRefName)) {
+                        countOfNodeVsIncomingNode[ruleRefName] = countOfNodeVsIncomingNode[ruleRefName] + 1;
+                    } else {
+                        countOfNodeVsIncomingNode[ruleRefName] = 0;
+                    }
+                })
+
+                if (countOfNodeVsIncomingNode.hasOwnProperty(nodeName)) {
+                    countOfNodeVsIncomingNode[nodeName] = countOfNodeVsIncomingNode[nodeName] + 1;
+                } else {
+                    countOfNodeVsIncomingNode[nodeName] = 0;
+                }
+
+                if (countOfNodeVsIncomingNode.hasOwnProperty(targetRefName)) {
+                    countOfNodeVsIncomingNode[targetRefName] = countOfNodeVsIncomingNode[targetRefName] + 1;
+                } else {
+                    countOfNodeVsIncomingNode[targetRefName] = 0;
+                }
+
+            })
+        }
+
+        if (Array.isArray(value)) {
+            Array.from(value).forEach(item => {
+                let targetRefName = item?.connector?.targetReference;
+                let nodeName = item?.name;
+
+                if (countOfNodeVsIncomingNode.hasOwnProperty(nodeName)) {
+                    countOfNodeVsIncomingNode[nodeName] = countOfNodeVsIncomingNode[nodeName] + 1;
+                } else {
+                    countOfNodeVsIncomingNode[nodeName] = 0;
+                }
+
+                if (countOfNodeVsIncomingNode.hasOwnProperty(targetRefName)) {
+                    countOfNodeVsIncomingNode[targetRefName] = countOfNodeVsIncomingNode[targetRefName] + 1;
+                } else {
+                    countOfNodeVsIncomingNode[targetRefName] = 0;
+                }
+
+            })
+        }
+
+        if (value.constructor.name === "Object") {
+            let targetRefName = value?.connector?.targetReference;
+            if (countOfNodeVsIncomingNode.hasOwnProperty(targetRefName)) {
+                countOfNodeVsIncomingNode[targetRefName] = countOfNodeVsIncomingNode[targetRefName] + 1;
+            } else {
+                countOfNodeVsIncomingNode[targetRefName] = 0;
+            }
+        }
+
+    }
+
+    return countOfNodeVsIncomingNode;
+}
+
+let countSample = analyzeFlowNodes(flowNode);
+
+console.log(countSample);
